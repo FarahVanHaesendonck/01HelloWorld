7e5271457b2dabdf2c327b2bc8dd2b34c014fdcf
Farah Van Haesendonck <farah.vanhaesendonck@student.romerocollege.be>
2019-10-02 21:08:36 +0200

    HTML en Visual Studio 2/10/2019

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿// 5AIT2 - 2019-09-10 - Hello World
+/* v1: Hello World op scherm tonen
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello World op scherm tonen
+            Console.WriteLine("Hello World!");
+            // wachten ...
+            Console.Write("Druk op Enter om af te sluiten.");
+            Console.ReadLine();
+          
+
+        }
+        
+    }
+}
